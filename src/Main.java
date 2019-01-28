96b655b9ec5c83f6d136aa64f6bad2a727f496c8
Semih-git <karakaplansemih@gmail.com>
2019-01-28 20:35:34 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
-    public void main(String[]args){
-        //sadasdasdassdsdfgsdg
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+        int age = scan.nextInt();
+        System.out.println("Please enter your name: ");
+        scan.nextLine();
+        String name = scan.nextLine();
+
+        System.out.println(String.format("Your age is %s and your name has %s characters", age, name.length()));
+
     }
+
 }
